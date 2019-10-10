49b084643a9bd723b14ede50ca6ecd1564c9fdb9
Lucas Melo <luucasmell@gmail.com>
2019-10-10 16:40:18 -0300

    Adicionar exemplo de userEffet e fetch

--- src/componentes/Cadastro/Cadastro.js
+++ src/componentes/Cadastro/Cadastro.js
@@ -1,21 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Input from "../Input";
 
 import "./Cadastro.scss";
+import Botao from "../Botao/Botao";
 
 const Cadastro = () => {
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [confirmEmail, setConfirmEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [id, setId] = useState(1);
+
+  const gerarPersonagem = () => {
+    if (id === 494) {
+      setId(1);
+    } else {
+      setId(id + 1)
+    }
+  }
 
   const handleSubmit = e => {
     e.preventDefault();
   };
 
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/${id}`, {
+      method: "GET"
+    }).then(result => {
+      return result.json()
+    }).then(data => {
+      console.log(data)
+    }).catch(() => {
+      console.error("Erroooooooou, internet ruim ou você que é")
+    })
+  }, [id])
+
   return (
     <div className="Cadastro">
       <h1>Faça seu cadastro</h1>
+      <button onClick={gerarPersonagem}>Gerar personagem</button>
       <form onSubmit={handleSubmit}>
         <Input
           value={nome}
@@ -23,7 +46,6 @@ const Cadastro = () => {
           label="Nome"
           placeholder="Nome completo"
           atualizarState={setNome}
-          obrigatorio
         />
         <Input
           value={email}
@@ -46,9 +68,8 @@ const Cadastro = () => {
           label="Senha"
           placeholder="Digite sua senha"
           atualizarState={setSenha}
-          obrigatorio
         />
-        <button>Cadastrar</button>
+        <Botao>Cadastrar</Botao>
       </form>
     </div>
   );
